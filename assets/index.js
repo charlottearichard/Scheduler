2a957f7b8d9d10a4526252e17b754b6ca72cb532
Charlotte <charlottealiciarichard@gmail.com>
2022-05-21 12:12:30 -0400

    Time, Input, and Save btn

--- assets/index.js
+++ assets/index.js
@@ -3,33 +3,62 @@ console.log ('javascript is running');
 //Current Day Display under title the 'ID' that is being called is connected to index.html
 var currentDayEl = document.getElementById("currentDay");
 
+
 // Moment JS is being used to display the date and time
 const setCurrentDay = () => {
     var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
-    currentDayEl.textContent = currentDay; 
-    
+    currentDayEl.textContent = currentDay;  
 };
 
+//Timeblocks 
+var timeBlocksEl = document.getElementById ("timeBlocks");
 
 
 
+//the containter that holds hours and events 
+const displayHours = () => {
+    for (i = 0; i < 9; i++) {
+      var hourBlockEl = document.createElement("div");
+      hourBlockEl.classList = "row";
+      timeBlocksEl.appendChild(hourBlockEl);
+      var hourTextEl = document.createElement("p");
+      
+      // shows hours of a typical teacher school day 7AM-3PM 
+      hourTextEl.classList = "col-2";
+      if (i + 7 < 12) {
+        hourTextEl.textContent = `${i + 7} AM`;
+      } else if (i + 7 == 12) {
+        hourTextEl.textContent = `${i + 7} PM`;
+      } else {
+        hourTextEl.textContent = `${i - 5} PM`;
+      }
 
-//timeslots according to workday hours 
-const showHours = () => {
-    for (i =0; i < 10; i++){
-        var hourBoxEl = document.createElement ("div"); 
-        hourBoxEl.classList = "row"; 
-        calenderEl.appendChild()
+      //input for events 
+      var eventEl = document.createElement("input");
+      eventEl.classList = "col-8"; // size of input box
+      
+      //Save buttons 
+      var saveBtnEl = document.createElement("button");
+      saveBtnEl.classList = "col-2 btn-primary";
+      saveBtnEl.textContent = "SAVE";
+      saveBtnEl.setAttribute("data-blockHour", i);
+      
+      //shows time on the left 
+      hourBlockEl.appendChild(hourTextEl);
+
+      //shows box for input 
+      hourBlockEl.appendChild(eventEl);
+
+      //shows save button 
+      hourBlockEl.appendChild(saveBtnEl);
     }
+  };
 
-}
-
-// Save buttons 
-
-// input 
 
 // color 
 
+// local storage 
+
 // this is the call to the current date & time
 setCurrentDay();
-
+displayHours();
